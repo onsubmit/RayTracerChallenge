7c5ad77a31a6c77dcc865675b01cbaa79b38572e
Andy <onsubmit@outlook.com>
2022-05-13 11:38:14 -0700

    Spruce up tuple tests a bit

--- TypeScript/test/Tuple.test.ts
+++ TypeScript/test/Tuple.test.ts
@@ -48,57 +48,66 @@ describe("Tuple", () => {
     const t1 = new Tuple(3, -2, 5, 1);
     const t2 = new Tuple(-2, 3, 1, 0);
 
-    expect(t1.add(t2).compare(new Tuple(1, 1, 6, 1))).toBe(true);
+    const sum = t1.add(t2);
+    expect(sum.compare(new Tuple(1, 1, 6, 1))).toBe(true);
   });
 
   it("Subtracting two points", () => {
     const p1 = new Point(3, 2, 1);
     const p2 = new Point(5, 6, 7);
 
-    expect(p1.subtract(p2).compare(new Vector(-2, -4, -6))).toBe(true);
+    const difference = p1.subtract(p2);
+    expect(difference.compare(new Vector(-2, -4, -6))).toBe(true);
   });
 
   it("Subtracting a vector from a point", () => {
     const p = new Point(3, 2, 1);
     const v = new Vector(5, 6, 7);
 
-    expect(p.subtract(v).compare(new Point(-2, -4, -6))).toBe(true);
+    const difference = p.subtract(v);
+    expect(difference.compare(new Point(-2, -4, -6))).toBe(true);
   });
 
   it("Subtracting two vectors", () => {
     const v1 = new Vector(3, 2, 1);
     const v2 = new Vector(5, 6, 7);
 
-    expect(v1.subtract(v2).compare(new Vector(-2, -4, -6))).toBe(true);
+    const difference = v1.subtract(v2);
+    expect(difference.compare(new Vector(-2, -4, -6))).toBe(true);
   });
 
   it("Subtracting a vector from the zero vector", () => {
     const v = new Vector(1, -2, 3);
 
-    expect(Vector.zero.subtract(v).compare(new Vector(-1, 2, -3))).toBe(true);
+    const negated = Vector.zero.subtract(v);
+    expect(negated.compare(new Vector(-1, 2, -3))).toBe(true);
   });
 
   it("Negating a tuple", () => {
     const t = new Tuple(1, -2, 3, -4);
 
-    expect(t.negate().compare(new Tuple(-1, 2, -3, 4))).toBe(true);
+    const negated = t.negate();
+    expect(negated.compare(new Tuple(-1, 2, -3, 4))).toBe(true);
   });
 
   it("Multiplying a tuple by a scalar", () => {
     const t = new Tuple(1, -2, 3, -4);
 
-    expect(t.multiply(3.5).compare(new Tuple(3.5, -7, 10.5, -14))).toBe(true);
+    const product = t.multiply(3.5);
+    expect(product.compare(new Tuple(3.5, -7, 10.5, -14))).toBe(true);
   });
 
   it("Multiplying a tuple by a fraction", () => {
     const t = new Tuple(1, -2, 3, -4);
 
-    expect(t.multiply(0.5).compare(new Tuple(0.5, -1, 1.5, -2))).toBe(true);
+    const product = t.multiply(0.5);
+    expect(product.compare(new Tuple(0.5, -1, 1.5, -2))).toBe(true);
   });
 
   it("Dividing a tuple by a scalar", () => {
     const t = new Tuple(1, -2, 3, -4);
 
-    expect(t.divide(2).compare(new Tuple(0.5, -1, 1.5, -2))).toBe(true);
+    const quotient = t.divide(2);
+    expect(quotient.compare(new Tuple(0.5, -1, 1.5, -2))).toBe(true);
   });
 });
